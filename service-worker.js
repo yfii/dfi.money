fbb4c0217b731011f6be8d37ddb2049f1907237b
beichenzhidi <13520648600@qq.com>
2021-04-07 09:50:48 +0000

    Deploying to gh-pages from  @ bdae361b5b93fde4a8adde01bc3619141b892496 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d7a9ff3c6ad9f6f6e8e28eccf5b312ec.js"
+  "/precache-manifest.a44299f6dfc9057a5ac92c666f0f07e6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
